e7e1d9bf4185e16902e5f2e5c724cbc04c9495b8
Axel <axel.ramirez@mexmrm-nmx0029.la.corp.ipgnetwork.com>
2019-01-02 11:39:49 -0600

    Se agrega respaldo de WP

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- Footer Section -->
+<footer>
+        <div class="container footer-container">
+            <div class="row">
+                <div class="col-xs-4 footer-col">
+                </div>
+                <div class="col-xs-4">
+                    <h2 class="footer-txt">Axel Ramírez Silva</h2>
+                </div>
+                <!--<div class="col-xs-4 footer-icons">
+                    <a href="https://www.linkedin.com/in/axel-ramírez-8054118b" target="_blank"><i class="fab fa-linkedin-in icon"></i></a>
+                    <a href="mailto:chelo3007@gmail.com"><i class="far fa-envelope icon"></i></a>
+                </div>-->
+            </div>
+        </div>
+    </footer>         
+    <script src="<?php bloginfo('template_url'); ?>/js/jquery-3.3.1.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/lightbox.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/popper.min.js"></script>
+    <script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es-en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Axel Portfolio</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/imgs/favicon-32x32.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/fontawesome-free/css/all.css">
+</head>
+<body>
+    <!-- Masthead Section -->
+    <div class="jumbotron-mh jumbotron-fluid">
+        <div class="d-flex justify-content-end">
+            <nav class="navbar navbar-expand-md navbar-light bg-transparent">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Menu de Navegacion">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link nav-txt" href="#aboutme">About me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-txt" href="#skills">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-txt" href="#work">Works</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link nav-txt" href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+        <div class="d-flex justify-content-center">
+            <h1 class="title-mh-txt"><strong>Hi I'AM AXEL RAMIREZ ENGINEER IN TECHNOLOGY</strong></h1>
+        </div>
+    </div>
